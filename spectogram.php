625d1fa9760ea35855a0ef573fceff60b928387e
Richard Beattie <richard.beattie@aol.co.uk>
2017-12-17 17:05:16 +0000

    Updated the design
    Updated the design to be responsive to different screen sizes. This
    involved adding bootstrap

--- spectogram.php
+++ spectogram.php
@@ -15,8 +15,11 @@
 	?>
 	<head>
 		<title>Spectogram</title>
-		<link rel="stylesheet" href="style.css?v=0.11">
-		<script src="jquery-3.2.1.min.js" type="text/javascript"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="dist/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/style2.css">
+		<script src="js/jquery.js" type="text/javascript"></script>
+		<script src="dist/js/bootstrap.min.js"></script>
 		<?php
 			echo("<script>var fileName='{$_GET['f']}';</script>");
 		?>
@@ -37,24 +40,43 @@
 		</script>
 	</head>
 	<body>	
-		<div class="content">
-			<a href="index.php" class="back-button">Back</a>
-			<h3 class="heading">Spectogram display</h3>
-			<div class="actions">
-				<img id="spectrogram-img" src="spec.png">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapseable">
+					 	<span class="sr-only">Toggle navigation</span>
+					 	<span class="icon-bar"></span>
+					 	<span class="icon-bar"></span>
+					 	<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand">Spectrogram display</a>
+				</div>
+				<div class="collapse navbar-collapse" id="collapseable">
+					<ul class="nav navbar-nav side-nav">
+						<h4>Audio files</h3>
+						<?php
+							$files = scandir(getcwd());
+							foreach($files as $key => $value){
+								if(strpos($value, ".wav") !== false){
+									echo("<li><a class='audiofile'>{$value}</a></li>");
+								}
+							}
+						?>
+					</ul>
+					<ul class="nav navbar-nav">
+						<li><a href="index.php">Home</a></li>
+					</ul>
+				</div>
 			</div>
-		</div>
-		<div class="side-bar">
-			<h3 class="subheading">Audio files</h3>
-			<a class="audiofile">Live</a>
-			<?php
-				$files = scandir(getcwd());
-				foreach($files as $key => $value){
-					if(strpos($value, ".wav") !== false){
-						echo("<a class='audiofile'>{$value}</a>");
-					}
-				}
-			?>
+		</nav>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-10">
+					<div class="content">
+						<img id="spectrogram-img" src="spec.png">
+					</div>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
--- timeExpansion.php
+++ timeExpansion.php
@@ -1,8 +1,11 @@
 <html>
 	<head>
 		<title>Time expansion</title>
-		<link rel="stylesheet" href="style.css?v=0.11">
-		<script src="jquery-3.2.1.min.js" type="text/javascript"></script>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link href="dist/css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/style2.css">
+		<script src="js/jquery.js" type="text/javascript"></script>
+		<script src="dist/js/bootstrap.min.js"></script>
 		<?php
 			echo("<script>var fileName='{$_GET['f']}';</script>");
 			if(isset($_GET['f'])){
@@ -29,26 +32,54 @@
 		</script>
 	</head>
 	<body>	
-		<div class="content">
-			<a href="index.php" class="back-button">Back</a>
-			<h3 class="heading">Time expansion</h3>
-			<div class="actions">
-				<a href="?stop"><button class="stop-button">Stop</button></a>
-				<img class="img-button" src="images/internal-speakers.png">
-				<img class="img-button" src="images/external-speakers.png">
+		<nav class="navbar navbar-default">
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#collapseable">
+					 	<span class="sr-only">Toggle navigation</span>
+					 	<span class="icon-bar"></span>
+					 	<span class="icon-bar"></span>
+					 	<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand">Time Expansion</a>
+				</div>
+				<div class="collapse navbar-collapse" id="collapseable">
+					<ul class="nav navbar-nav">
+						<li><a href="index.php">Home</a></li>
+					</ul>
+					<ul class="nav navbar-nav side-nav">
+						<h4>Audio files</h3>
+						<?php
+							$files = scandir(getcwd());
+							foreach($files as $key => $value){
+								if(strpos($value, ".wav") !== false){
+									echo("<li><a class='audiofile'>{$value}</a></li>");
+								}
+							}
+						?>
+					</ul>
+				</div>
 			</div>
-		</div>
-		<div class="side-bar">
-			<h3 class="subheading">Audio files</h3>
-			<a class="audiofile">Live</a>
-			<?php
-				$files = scandir(getcwd());
-				foreach($files as $key => $value){
-					if(strpos($value, ".wav") !== false){
-						echo("<a class='audiofile'>{$value}</a>");
-					}
-				}
-			?>
+		</nav>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-sm-10">
+					<div class="content actions">
+						<div class="row output-options">
+							<div class="col-sm-6">
+								<img class="img-button" src="images/internal-speakers.png">
+							</div>
+							<div class="col-sm-6">
+								<img class="img-button" src="images/external-speakers.png">
+							</div>
+						</div>
+						<div> 
+						<a id='speaker-status'>Current: Internal Speakers</a>
+						<a href="?stop"><button class="stop-button">Stop</button></a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>

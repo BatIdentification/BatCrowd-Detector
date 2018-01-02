ae2f329b3d644a84cf1184daa5318a7b63dae546
Richard Beattie <richard.beattie@aol.co.uk>
2018-01-02 17:01:00 +0000

    Fixed issue with ordering of audiofiles as you use the next an previous button

--- js/default.js
+++ js/default.js
@@ -3,6 +3,7 @@ var audioPage = 0;
 var foundPlayingFile = false;
 var fileName = "";
 var staticElements = 1;
+pages = [];
 
 //Helper Functions
 function tellBatPiTime(){
@@ -16,7 +17,7 @@ function addPageButtons(){
 	sideNav = $(".side-nav")[0];
 	arrLength = $("li:not('.hidden') > .audiofile").length;
 	//Check if there are more files than the height of the bar
-	if(sideNav.offsetHeight < sideNav.scrollHeight){
+	if(sideNav.offsetHeight < sideNav.scrollHeight + 10){
 		$("li:not('.hidden') > .audiofile").each(function(e){
 			//Check if a file is currently playing
 			if(fileName != "" && foundPlayingFile == false){	
@@ -33,7 +34,8 @@ function addPageButtons(){
 				//Runs if we havn't found the file yet, if the playing audio file is the not going to be visible. Because this function found it, it will stop here but we need it to go forward one more page to see the audiofile
 				if(fileName != "" && foundPlayingFile == false || fileName == this.innerHTML || foundElement == lastElement){
 					audioPage += 1;
-					$(lastElement).parent().prevAll().slice(0, -2).addClass("hidden");
+					pages.push($(lastElement).parent().prevAll(":not(.hidden)").slice(0, -staticElements))
+					$(lastElement).parent().prevAll().slice(0, -staticElements).addClass("hidden");	
 				}else{
 					tabLi = audioPage == 0 ? $("<li class='center-text'><a class='pageControl' id='nextPage'>Next</a></li>") : $("<li class='center-text'><a class='pageControl' id='previousPage'>Previous</a><a class='pageControl' id='nextPage'>Next</a></li>")
 					$(tabLi).insertBefore($(lastElement).parent());	
@@ -49,43 +51,31 @@ function addPageButtons(){
 		});					
 	}
 	if(audioPage != 0 && $(".pageControl").length == 0){
-		console.log(audioPage);
 		$("<li class='center-text bottom-of-div'><a class='pageControl' id='previousPage'>Previous</a></li>").insertAfter($("li:not('.hidden') > .audiofile").last().parent());
 	}
 }
 //Page stuff
 $(document).ready(function(){
 	//Code for setting up the audio-files side-bar
-	$(document).on("click", "a#nextPage" , function() {
-		$(this).parent().prevAll().slice(0, -staticElements).addClass("hidden");         		
-		$(this).parent().remove();
+	$(document).on("click", "a#nextPage" , function(){
+		if(pages[audioPage] == undefined){
+			pages.push($(this).parent().prevAll(":not(.hidden)").slice(0, -staticElements));	
+			$(this).parent().prevAll(":not(.hidden)").slice(0, -staticElements).addClass("hidden");     
+		}else{
+			pages[audioPage].each(function(){
+				$(this).addClass("hidden");
+			})
+		}
 		audioPage += 1;
+		$(this).parent().remove();
 		addPageButtons();
 	});
 	$(document).on("click", "a#previousPage", function(){
-		i = 0
-		prevBottom = undefined;
-		//Gets the first(visible) element under audiofiles
-		topFile = $(this).parent().prevAll().slice(0,-staticElements).filter(":not(.hidden)").last()[0]
-		topRect = topFile.getBoundingClientRect();
-		//While this element is able to be seen do this
-		while(topRect.bottom < sideNav.offsetHeight){
-			//Remove the hidden class from the first non-visible element
-			$(topFile).prevAll("li").eq(i).removeClass("hidden")
-			//Get this elements position again
-			topRect = topFile.getBoundingClientRect();
-			i += 1
-			//topRect has stopped moving downwards
-			if(prevBottom == topRect.bottom){
-				break
-			}
-			prevBottom = topRect.bottom;
-		}
-		//Due to how this method works(pushing down the audiofile) we get one extra audiofile than what we can have(since we will be adding the pages buttons), therefore we will hide the last audiofile that we added.
-		$(topFile).prevAll("li > a.audiofile").eq(i-1).addClass("hidden")
-		console.log(i);
-		$(this).parent().remove();
 		audioPage -= 1;
+		pages[audioPage].each(function(){
+			$(this).removeClass("hidden");
+		});
+		$(this).parent().remove();
 		addPageButtons();
 	});
 });

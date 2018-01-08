0672106b01d496dbd59675ca47b599753a2ce475
Richard Beattie <richard.beattie91@gmail.com>
2018-01-08 11:31:50 +0000

    Changed to absolute path of shutdown command

--- commands.php
+++ commands.php
@@ -2,6 +2,6 @@
 	if(isset($_POST['time'])){
 		shell_exec("sudo /bin/date -s '{$_POST['time']}'");
 	}elseif(isset($_POST['shutdown'])){
-		shell_exec("sudo shutdown now");
+		shell_exec("sudo /sbin/shutdown now");
 	}
 ?>

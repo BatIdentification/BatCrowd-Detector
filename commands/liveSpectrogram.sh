822afd14039e061e9c9a6d8018a03e77edad942b
Richard Beattie <richard.beattie@aol.co.uk>
2018-06-15 17:17:21 +0100

    Performance improvements, higher quality time expansion, default spectrogram
    The spectrogram pages only refreshes the spectrogram when their is a
    new one or when live spectrogram is enabled, increases the gain of
    time-expansion calls to make the pips more audible, and creates a
    default spectrogram image

--- commands/liveSpectrogram.sh
+++ commands/liveSpectrogram.sh
@@ -2,6 +2,7 @@
 
 SECONDS=0
 while [[ SECONDS -lt 300 ]]; do
-	arecord -Dhw:sndrpiwsp -r 192000 -c 2 -f S16_LE -d4 /var/www/audiofiles/liveSpec.wav & wait
-	sox /var/www/audiofiles/liveSpec.wav -n remix 1 rate 192k spectrogram -o /var/www/spec.png sinc 10k & wait
+	today=$(date '+%F-%H-%M-%S')
+	arecord -Dhw:sndrpiwsp -r 192000 -c 2 -f S16_LE -d4 /var/www/audiofiles/$today.wav & wait
+	sox /var/www/audiofiles/$today.wav -n remix 1 rate 192k spectrogram -o /var/www/$today.png sinc 10k & wait
 done
--- commands/timeExpansion.sh
+++ commands/timeExpansion.sh
@@ -1,9 +1,9 @@
 amixer sset 'Speaker Digital' 100%
 
 if [ $1 = "Live" ]; then
-	( arecord -Dhw:sndrpiwsp -r 192000 -c 2 -f S16_LE | sox -t wav -e signed-integer -b16 -r 192000 -c 2 /dev/stdin -t wav -e signed-integer -b16 -r 192000 -c 2 /dev/stdout sinc 10k speed 0.1 | aplay -D hw:sndrpiwsp -) &> /dev/null	
+	( arecord -Dhw:sndrpiwsp -r 192000 -c 2 -f S16_LE | sox -t wav -e signed-integer -b16 -r 192000 -c 2 /dev/stdin -t wav -e signed-integer -b16 -r 192000 -c 2 /dev/stdout sinc 10k speed 0.1 gain -n -7 | aplay -D hw:sndrpiwsp -) &> /dev/null
 elif [ -f "time-expansion-audio/$1" ]; then
 	(aplay -q "time-expansion-audio/$1") &
 else
-	(sox -t wav -e signed-integer -b16 -r 192000 -c 2 $1 -t wav -e signed-integer -b16 -r 192000 -c 2 /dev/stdout sinc 10k speed 0.1 |  aplay -D hw:sndrpiwsp -) &> /dev/null
+	(sox -t wav -e signed-integer -b16 -r 192000 -c 2 $1 -t wav -e signed-integer -b16 -r 192000 -c 2 /dev/stdout sinc 10k speed 0.1 gain -n -7 |  aplay -D hw:sndrpiwsp -) &> /dev/null
 fi

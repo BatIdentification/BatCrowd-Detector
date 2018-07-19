e1d0bad6e3830c3e8ae4ef5a0b9c329d24441a1f
Richard Beattie <richard.beattie@aol.co.uk>
2018-07-19 14:19:30 +0100

    Asychronous request for networks
    Moved the php code to return the list of networks to endpoint.php so that settings.php can call this
    asychronously

--- endpoint.php
+++ endpoint.php
@@ -8,4 +8,31 @@
 
   }
 
+  if(isset($_POST['networks'])){
+
+    $networks = []
+
+    $command = "sudo /var/www/libraries/iw-4.9/iw wlan0 scan ap-force | grep SSID";
+    $network_from_command = shell_exec($command);
+    $available_networks = explode('SSID: ', $network_from_command);
+    unset($available_networks[0]);
+    $currentNetwork = shell_exec("/sbin/iwgetid");
+    $i = 0;
+    foreach($available_networks as $network){
+      $ssid = substr($network, 0, -1);
+      $ssid = rtrim($ssid);
+      $isKnown = shell_exec("cat /etc/wpa_supplicant/wpa_supplicant.conf | grep '$ssid'");
+      $status = ($isKnown == "" ? "New network" : "Not connected");
+      $status = (strpos($currentNetwork, $ssid) !== false ? "Connected" : $status);
+      $action = "";
+      if($status != "Connected"){
+        $action = ($status == "New network" ? "Add Network" : "Connect");
+      }
+      $networks[] = [$ssid, $status, $action];
+    }
+
+    echo("{networks: " . $networks . '}');
+
+  }
+
 ?>
--- settings.php
+++ settings.php
@@ -2,10 +2,7 @@
 	<?php
 		shell_exec("pkill -f /bin/bash\ commands/liveSpectrogram.sh");
  		if(isset($_POST['password']) && isset($_POST['ssid'])){
-			$newConfig = 'network={
-	ssid="'.$_POST['ssid'].'"
-	psk="'.$_POST['password'].'"
-}';
+			$newConfig = 'network={ ssid="'.$_POST['ssid'].'" psk="'.$_POST['password'].'"}';
 			$file = "/etc/wpa_supplicant/wpa_supplicant.conf";
 			file_put_contents($file, $newConfig, FILE_APPEND);
 		}
@@ -23,11 +20,14 @@
 			$(document).ready(function(){
 				$(".new-network, .popup .btn-danger").click(function(){
 					$(".popup").toggle();
-					$("#ssid").val($(this).parents("tr").children().eq(0).text());					
+					$("#ssid").val($(this).parents("tr").children().eq(0).text());
 				})
 				$(".setting-option:not(.seleted)").click(function(){
 					 $.post("commands.php", {toggleWifi: true});
 				});
+				$.post('endpoint.php', {networks: true}, function(data){
+					console.log(data);
+				})
 			})
 		</script>
 	</head>
@@ -72,27 +72,7 @@
 											<th>Status</th>
 											<th>Action</th>
 										</tr>
-										<?php
-											$command = "sudo /var/www/libraries/iw-4.9/iw wlan0 scan ap-force | grep SSID";
-											$network_from_command = shell_exec($command);
-											$available_networks = explode('SSID: ', $network_from_command);
-											unset($available_networks[0]);
-											$currentNetwork = shell_exec("/sbin/iwgetid"); 
-											$i = 0;
-											foreach($available_networks as $network){
-												$ssid = substr($network, 0, -1);
-												$ssid = rtrim($ssid);
-												$isKnown = shell_exec("cat /etc/wpa_supplicant/wpa_supplicant.conf | grep '$ssid'");
-												$status = ($isKnown == "" ? "New network" : "Not connected");
-												$status = (strpos($currentNetwork, $ssid) !== false ? "Connected" : $status);
-												$action = "";
-												if($status != "Connected"){
-													$action = ($status == "New network" ? "Add Network" : "Connect");
-												}
-												echo("<tr><td>{$ssid}</td><td>{$status}</td><td><a class='new-network'>{$action}</a></td></tr>");
-											}
-
-										?>
+										<tr><td>{$ssid}</td><td>{$status}</td><td><a class='new-network'>{$action}</a></td></tr>
 								</table>
 							</div>
 						</div>

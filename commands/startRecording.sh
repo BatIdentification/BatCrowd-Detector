4ee55646c5e04f0932e52a1fab6e6d27b8f0d7ad
Richard Beattie <richard.beattie@aol.co.uk>
2019-04-13 17:07:59 +0100

    Calls added automatically
    Calls are now added to the database as they are recorded

--- commands/startRecording.sh
+++ commands/startRecording.sh
@@ -2,4 +2,5 @@ export AUDIODEV=hw:0,0
 export AUDIODRIVER=alsa
 
 today=$(date '+%F-%H-%M-%S')
+curl -d "date_recorded=$today&url=$today.wav" -X POST https://batpi.loc/endpoints/calls.php
 rec -c 2 -r 192000 audiofiles/$today.wav sinc 10k & > log.txt
--- commands/startSoundActivatedRecording.sh
+++ commands/startSoundActivatedRecording.sh
@@ -5,5 +5,6 @@ while :
 do
 	today=$(date '+%F-%H-%M-%S')
 	rec -q -c 2 -r 192000 audiofiles/$today.wav sinc 10k silence 1 0.1 0.125% 1 2.0 0.125% & wait > log.txt
+	curl -d "date_recorded=$today&url=$today.wav" -X POST https://batpi.loc/endpoints/calls.php
 	sleep 0.1s
 done
